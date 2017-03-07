4d9fb0456915e88ae15f46d50e480af92d976960
Cesar Palafox <cpaguila1@gmail.com>
2017-03-06 21:06:54 -0600

    Adds basic object functionality

--- lib/rate_exchange/money.rb
+++ lib/rate_exchange/money.rb
@@ -1,5 +1,15 @@
 module RateExchange
   class Money
+    attr_reader :amount, :currency
+
+    def initialize(amount, currency)
+      @amount = amount
+      @currency = currency
+    end
+
+    def inspect
+      "#{format('%.2f', amount)} #{currency}"
+    end
 
     def self.conversion_rates(base_currency, conversion_rates)
       @@rates = { base_currency => conversion_rates }
--- spec/lib/rate_exchange/money_spec.rb
+++ spec/lib/rate_exchange/money_spec.rb
@@ -1,6 +1,7 @@
 require "spec_helper"
 
 describe RateExchange::Money do
+  let(:money) { RateExchange::Money.new(50, 'EUR') }
 
   describe '.conversion_rates' do
     it 'configures class variable' do
@@ -11,6 +12,19 @@ describe RateExchange::Money do
 
       expect(RateExchange::Money.rates).to be_a Hash
       expect(RateExchange::Money.rates).to eq({ 'EUR' => { 'USD' => 1.11, 'Bitcoin' => 0.0047 } })
+    end
+  end
+
+  describe '#initialize' do
+    it 'has access to amount and currency' do
+      expect(money.amount).to eq(50)
+      expect(money.currency).to eq('EUR')
+    end
+  end
+
+  describe '#inspect' do
+    it 'returns a string with the amount and currency' do
+      expect(money.inspect).to eq "50.00 EUR"
     end
   end
 end
